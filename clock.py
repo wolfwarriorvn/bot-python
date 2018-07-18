f2a6f6554d7b798a195a02cb1aa6e9f94a01ab04
Cody Nguyen <vietcuong2892@gmail.com>
2018-07-18 21:08:44 +0700

    dissable interval task

--- clock.py
+++ clock.py
@@ -3,10 +3,10 @@ from app import *
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', seconds=60)
-def timed_job():
-    print('This job is run every three seconds.')
-    # bot_test()
+# @sched.scheduled_job('interval', seconds=60)
+# def timed_job():
+#     print('This job is run every three seconds.')
+#     # bot_test()
 
 # @sched.scheduled_job('cron', day_of_week='mon-sun', hour=7)
 # def scheduled_job():
@@ -17,5 +17,6 @@ def timed_job():
 @sched.scheduled_job('cron', hour=7)
 def timed_job_one():
     print("Test every 7h")
-
+    bot_run()
+    
 sched.start()
